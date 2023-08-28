2e0eb14cec82b529219fa3a45ff0b73539717884
Blanka Semanova <bsemanova@deloittece.com>
2023-08-28 13:25:21 +0200

    Add Next UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { NextUIProvider, Button } from '@nextui-org/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Habit tracker app</p>
-      </header>
-    </div>
+    <NextUIProvider>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="text-3xl font-bold underline">
+            Habit tracker
+          </h1>
+          <Button color='primary'>Text NextUI</Button>
+        </header>
+      </div>
+    </NextUIProvider>
   );
 }
 
