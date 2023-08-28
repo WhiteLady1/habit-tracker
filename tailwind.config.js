f87474aee796b72057039fbf3cf40d16d53a812c
Blanka Semanova <bsemanova@deloittece.com>
2023-08-28 13:43:05 +0200

    fix import in the tailwinf config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-const {nextui} = require("@nextui-org/react");
+import { nextui } from "@nextui-org/react";
 
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [nextui()],
-}
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const darkMode = "class";
+export const plugins = [nextui()];
 
